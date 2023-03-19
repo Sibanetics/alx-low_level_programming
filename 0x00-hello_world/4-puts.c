10e580278c57c7e5c189b41d0c67a0d8092c0d30
Boko Isaac <bokoisaac15@gmail.com>
2023-03-19 19:27:57 +0000

    This program prints a string/array of character, it took me long but I later got here

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Input and Outputing data
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
