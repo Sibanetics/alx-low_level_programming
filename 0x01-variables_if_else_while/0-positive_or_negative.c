31b4e0fc5e019ec0b3478dd8e609f9c409bec17b
Boko Isaac <bokoisaac15@gmail.com>
2023-03-22 19:26:28 +0000

    This program stores a random number and states if the number is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,8 +11,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	scanf("%d\n", &n);
-
 	if (n > 0)
 	{
 		printf("%d is postive\n", n);
@@ -25,7 +23,7 @@ int main(void)
 
 	else
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
