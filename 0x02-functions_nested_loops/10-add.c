b0bd9bd3b6ff736e01d512455e3457a13072aca4
Boko Isaac <bokoisaac15@gmail.com>
2023-04-01 15:14:54 +0100

    This program prints the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ * @i: parameter that denotes number
+ * @j: parameter that denotes number
+ *
+ * Return: Always result
+ */
+
+int add(int i, int j)
+{
+	int result = i + j;
+
+	return (result);
+}
+
