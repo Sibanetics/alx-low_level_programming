9fc684dc52612fb26040ee20477ad38c61fac602
Boko Isaac <bokoisaac15@gmail.com>
2023-04-25 12:27:21 +0100

    This program prints all argumens recived

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints all arhuments it recieves
+ * @argc: number of arguments passed
+ * @argv: pointer to array of size argc 
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
